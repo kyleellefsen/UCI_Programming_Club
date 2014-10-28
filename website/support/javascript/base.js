e063301398bf075f3521a3029144921bae611a14
kyleellefsen <kellefse@uci.edu>
2014-10-27 17:10:00 -0700

    updated website

--- website/support/javascript/base.js
+++ website/support/javascript/base.js
@@ -1,4 +1,14 @@
 $(init);
 function init() {
     $( ".tabs" ).tabs();
+    $('.toptab').tabs({ active: 1 });
+    
+    var now = new Date();
+    var start = new Date(2014, 8, 28); // September 28th
+    var diff = now - start;
+    var oneDay = 1000 * 60 * 60 * 24;
+    var day = Math.floor(diff / oneDay);
+    var week = Math.floor(day/7);
+    $('#weeks').find("li.ui-state-active").removeClass('ui-state-active');
+    $('#weeks').tabs({ active: week-1 });
 }
