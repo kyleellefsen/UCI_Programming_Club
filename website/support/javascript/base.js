534f89b851752443ca866c7e03d7c11995364fbe
kyleellefsen <kellefse@uci.edu>
2015-04-06 11:01:12 -0700

    Spring 2015 beginning

--- website/support/javascript/base.js
+++ website/support/javascript/base.js
@@ -4,11 +4,11 @@ function init() {
     $('.toptab').tabs({ active: 1 });
     
     var now = new Date();
-    var start = new Date(2014, 8, 28); // September 28th
+    var start = new Date(2015, 3, 5); // April 6th start date
     var diff = now - start;
     var oneDay = 1000 * 60 * 60 * 24;
     var day = Math.floor(diff / oneDay);
     var week = Math.floor(day/7);
     $('#weeks').find("li.ui-state-active").removeClass('ui-state-active');
-    $('#weeks').tabs({ active: week-1 });
+    $('#weeks').tabs({ active: week });
 }
