fb86ae21cbfbbad7e30ba22068bc02f18eb4570d
Kyle Ellefsen <kellefse@uci.edu>
2015-10-18 20:22:58 -0700

    Now we're in Fall 2015

--- 2015_spring_week5/kyles_solution.py
+++ 2015_spring_week5/kyles_solution.py
@@ -10,6 +10,12 @@ import matplotlib.pyplot as plt #this is the package that reads in images and ca
 import os
 import numpy as np
 
+os.chdir('/Users/kyle/Github/ParkerLab/FLIKA/PyFLIKA2/')
+from FLIKA import *
+app = QApplication(sys.argv)
+initializeMainGui()
+
+
 image_file_path='/Users/kyle/Github/UCI_Programming_Club/2015_spring_week5/mouse video/' #Replace this with your own path to the image directory
 os.chdir(image_file_path) #now we are inside the directory which contains the images
 file_names=os.listdir('.') #This creates a list containing all the files in the current directory.  The dot '.' represents the current directory
@@ -28,3 +34,77 @@ plt.imshow(image)
 background=np.median(images,0) 
 background_subtracted=images-background
 thresholded=background_subtracted<-40
+
+import numpy as np
+import scipy
+from scipy.ndimage.measurements import label
+from process.file_ import open_file
+from window import Window
+    
+def keepLargestBinary(image): # replace with skimage.morphology.remove_small_objects
+    s=scipy.ndimage.generate_binary_structure(2,2)
+    for i in np.arange(len(image)):
+        labeled_array, num_features = label(image[i], structure=s)
+        if num_features>1:
+            lbls = np.arange(1, num_features+1)
+            largest=0
+            largest_lbl=0
+            for lbl in lbls:
+                length=np.count_nonzero(labeled_array==lbl)
+                if length>largest:
+                    largest=length
+                    largest_lbl=lbl
+            image[i]=labeled_array==largest_lbl
+    return image
+
+original=g.m.currentWindow
+nFrames=len(original.image)
+median=zproject(0,nFrames,'Median',keepSourceWindow=True)
+original.image-=median.image
+g.m.currentWindow=original
+gaussian_blur(2)
+threshold(-10,darkBackground=True)
+image=keepLargestBinary(g.m.currentWindow.image)
+Window(image)
+
+
+#filename="D:/Old/Software/2014.05 motiontracking/t1_1.tif"
+#original=open_file(g.m.settings['filename'])
+
+
+"""
+from PyQt4.QtCore import pyqtSlot as Slot
+@Slot(int)
+def updateTime(time):
+    x0,y0,x1,y1=bbox[time]
+    roi=g.m.currentWindow.currentROI
+    roi.draw_from_points([(x0,y0),(x0,y1),(x1,y1),(x1,y0),(x0,y0)])
+    roi.translate_done.emit()
+    print(time)
+bbox=np.zeros((len(image),4))
+for i in np.arange(len(image)):
+    B = np.argwhere(image[i])
+    bbox[i,:]=np.concatenate((B.min(0),B.max(0)))
+original.imageview.sigTimeChanged.connect(updateTime)
+background=np.copy(g.m.currentWindow.image)
+for i in np.arange(len(background)):
+    x0,y0,x1,y1=bbox[i]
+    for j in np.arange(i,len(background)): #j is the first index where the bounding boxes don't collide
+        left,top,right,bottom=bbox[j]
+        if (left>x1 or top>y1 or bottom<y0 or right<x0): #if the bounding boxes to not intersect
+            break
+    if j==len(background)-1:
+        for j in np.arange(i,0,-1): #j is the first index where the bounding boxes don't collide
+            left,top,right,bottom=bbox[j]
+            if (left>x1 or top>y1 or bottom<y0 or right<x0): #if the bounding boxes to not intersect
+                break
+    background[i,x0:x1,y0:y1]=background[j,x0:x1,y0:y1]
+show(background)
+image=np.copy(g.m.currentWindow.image)
+g.m.currentWindow.image=image-background; g.m.currentWindow.reset()
+threshold(-27,darkBackground=True)
+binary_dilation(2,1,1,keepSourceWindow=True)
+g.m.currentWindow.image=keepLargestBinary(g.m.currentWindow.image)
+g.m.currentWindow.reset()
+"""
+    
--- website/support/javascript/base.js
+++ website/support/javascript/base.js
@@ -4,11 +4,13 @@ function init() {
     $('.toptab').tabs({ active: 1 });
     
     var now = new Date();
-    var start = new Date(2015, 3, 5); // April 6th start date
+    var start = new Date("2015-09-20 GMT-0700"); // September 20th
     var diff = now - start;
     var oneDay = 1000 * 60 * 60 * 24;
     var day = Math.floor(diff / oneDay);
     var week = Math.floor(day/7);
-    $('#weeks').find("li.ui-state-active").removeClass('ui-state-active');
-    $('#weeks').tabs({ active: week });
+    if (week>0){
+        $('#weeks').find("li.ui-state-active").removeClass('ui-state-active');
+        $('#weeks').tabs({ active: week-1 });
+    }
 }
