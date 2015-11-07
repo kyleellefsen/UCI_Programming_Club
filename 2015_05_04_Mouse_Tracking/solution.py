e1c2f9c3cbf7f7da7a0e7df4fbac9609b057ffff
kyleellefsen <kellefse@uci.edu>
2015-11-07 15:39:43 -0800

    updated website

--- 2015_05_04_Mouse_Tracking/solution.py
+++ 2015_05_04_Mouse_Tracking/solution.py
@@ -1,29 +1,80 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon May  4 09:04:53 2015
+Created on Fri May 08 20:11:28 2015
 
+@author: Carley
 """
 
 from __future__ import division
-import matplotlib.pyplot as plt #this is the package that reads in images and can display them
+import matplotlib.pyplot as plt 
 import os
 import numpy as np
+from skimage.measure import label
+from skimage.measure import regionprops
 
-image_file_path='/Users/kyle/Github/UCI_Programming_Club/2015_spring_week5/mouse video/' #Replace this with your own path to the image directory
-os.chdir(image_file_path) #now we are inside the directory which contains the images
-file_names=os.listdir('.') #This creates a list containing all the files in the current directory.  The dot '.' represents the current directory
-images=[]
-for file_name in file_names:
-  image=plt.imread(file_name)
-  image=np.mean(image,2) # There are 3 dimensions, [x,y,color].  This averages over the third dimension.  
-  images.append(image)
-images=np.array(images)  
-''' Now we have an array of images.  The array has dimensions [t,x,y].  We can pull out the first image and play with it. '''
-image=images[0]
-plt.gray()
-plt.imshow(image)
+def calculate_background(short_list):
+	'''uses the first 100 frames to calculate the median (background)'''
+    bkgd=[]
+    for files in short_list:
+        image=plt.imread(os.path.join(direc,videos,files))
+        image=np.mean(image,2)
+        bkgd.append(image)
+    return np.median(bkgd,axis=0)
 
-''' Lets find the background by taking the median image '''
-background=np.median(images,0) 
-background_subtracted=images-background
-thresholded=background_subtracted<-40
+def get_coordinates(single_jpeg):
+	''' '''
+    coordinate_list=[]
+    image=plt.imread(os.path.join(direc,videos,single_jpeg))
+    image=np.mean(image,2)
+    frame=image-background
+    thresh=frame<-50
+    labelled=label(thresh)
+    numObjects=np.max(labelled)
+    props=regionprops(labelled)
+    indices=[]
+    for x in range(numObjects):
+        indices.append((x,props[x]['area']))
+    largest_object=sorted(indices,key=lambda index:index[1],reverse=True)[0]
+    #this line will sort by area so that the largest object is first in the list
+    #then you can find the index of that object and pull its centroid coordinates
+    i=largest_object[0]
+    coordinate_list.append(list(props[i]['centroid']))  
+    return coordinate_list
+
+def chamber_counts(coordinates):
+  	''' '''
+    xs=[]
+    for items in coordinates:
+        xs.append(items[1])
+    positions=[]
+    width=np.max(xs)
+    divs=width/3
+    rightBound=width-divs
+    leftBound=width-(2*divs)
+    left=0
+    center=0
+    right=0
+    for items in xs:
+        if items>rightBound:
+            right+=1
+        if items<leftBound:
+            left+=1
+        else:
+            center+=1
+    positions.append(left)
+    positions.append(center)
+    positions.append(right)
+    return positions    
+
+direc=r'D:\150508 OD pilot\testing'
+folders=os.listdir(direc)
+results_all=[]
+for videos in folders:
+    print videos
+    files = os.listdir(direc+'\\'+videos)
+    background = calculate_background(files[:100])
+    results = []
+    for jpgs in files:
+        coords = get_coordinates(jpgs)
+        results.append(coords)
+    results_all.append(results)
